10c6ee6816ec7d450283fa17fff9cab055bc8d23
Florian Hacqueville-Mathi <florianhacqueville-mathi@iMac-de-Florian.local>
2023-04-13 19:21:16 +0200

    correction button

--- FrontEnd/assets/projet.js
+++ FrontEnd/assets/projet.js
@@ -1,6 +1,7 @@
 // Récupération des projets depuis le fichier JSON
 const reponse = await fetch('http://localhost:5678/api/works/');
 const projet = await reponse.json();
+const buttonFilter = document.querySelectorAll(".btn-filter");
 
 function genererProjet(projet){
     for (let i = 0; i < projet.length; i++) {
@@ -24,28 +25,24 @@ function genererProjet(projet){
 
 genererProjet(projet);
 
-const buttonFilter = document.querySelectorAll(".btn-filter");
 for (let x = 0; x < buttonFilter.length; x++){
 	buttonFilter[x].addEventListener("click", boutonFiltrerTous);
-
 }
 
 function boutonFiltrerTous(clickEvent) {
     const classButtonFilter = clickEvent.target;
+    classButtonFilter.classList.add("active");
     const projetFiltreesHotels = projet.filter(function (projet) {
-        if (classButtonFilter.classList.value === "btn-filter btn-tous"){
-            
-            return projet.categoryId;
-        } else if(classButtonFilter.classList.value === "btn-filter btn-objets") {
-            return  projet.categoryId <=1;
-        } else if(classButtonFilter.classList.value === "btn-filter btn-appartements"){
-            return  projet.categoryId <=2;
-        } else {
-            return  projet.categoryId <=3;
+        if (classButtonFilter.classList.value === "btn-filter btn-tous active" || classButtonFilter.classList.value === "btn-filter btn-tous"){   
+            return  projet.categoryId;
+        } else if(classButtonFilter.classList.value === "btn-filter btn-objets active" || classButtonFilter.classList.value === "btn-filter btn-objets") {
+            return  projet.categoryId ===1;
+        } else if(classButtonFilter.classList.value === "btn-filter btn-appartements active" || classButtonFilter.classList.value === "btn-filter btn-appartements"){
+            return  projet.categoryId ===2;
+        } else if (classButtonFilter.classList.value === "btn-filter btn-hotels active"|| classButtonFilter.classList.value === "btn-filter btn-hotels"){
+            return  projet.categoryId ===3;
         }
     });
-    classButtonFilter.classList.add("active");
-    console.log(classButtonFilter)
     document.querySelector(".gallery").innerHTML = "";
     genererProjet(projetFiltreesHotels);
 }
